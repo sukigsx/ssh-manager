73a73244c3163915af49eb8a7ee3fe9864db4595
sukigsx <sukigsx.mbsistemas@gmail.com>
2026-08-14 11:00:37 +0200

    Update ssh-manager.sh

--- ssh-manager.sh
+++ ssh-manager.sh
@@ -43,7 +43,7 @@ fi
 software_necesario(){
 var_software="no"
 echo -e " Verificando software necesario:"
-software="which git diff ping apt fzf curl jq nc ssh-copy-id  ssh-keygen awk sed" #ponemos el foftware a instalar separado por espacion dentro de las comillas ( soft1 soft2 soft3 etc )
+software="which git diff ping fzf curl jq nc ssh-copy-id  ssh-keygen awk sed" #ponemos el foftware a instalar separado por espacion dentro de las comillas ( soft1 soft2 soft3 etc )
 for paquete in $software
 do
 which $paquete 2>/dev/null 1>/dev/null 0>/dev/null #comprueba si esta el programa llamado programa
